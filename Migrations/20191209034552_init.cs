2c6638144c8e6f7b4737f7c4485c646e4391639b
YUYUNWU <yuyunwu@gapps.uch.edu.tw>
2019-12-09 12:21:14 +0800

    完成 1-4 項：每一個資料表皆已新增完Controllers
    1.contosoUniversity 資料庫為主要資料來源
    2.透過 DB First 流程建立 EF Core 實體資料模型
    3.對資料庫進行版控 (使用資料庫移轉方式)
    4,對每一個表格設計出完整的 CRUD 操作 APIs

---
+++ Migrations/20191209034552_init.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeWork1.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
