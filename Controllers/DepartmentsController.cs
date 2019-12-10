16bc854962999596b7d43d57a354ea77e0eed13d
YUYUNWU <yuyunwu@gapps.uch.edu.tw>
2019-12-10 18:25:36 +0800

    完成Department的CUD使用Stored Procedure

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HomeWork1.models;
+using Microsoft.Data.SqlClient;
 
 namespace HomeWork1.Controllers
 {
@@ -52,23 +53,42 @@ namespace HomeWork1.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(department).State = EntityState.Modified;
+            //原本的PUT的內容
+            //_context.Entry(department).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DepartmentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!DepartmentExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+            //return NoContent();
+            //原本的PUT的內容
+
+
+            //使用Stored Procedure 進行修改資料
+
+            // var id1 = new SqlParameter("@DepartmentID", department.DepartmentId);
+            var id1 = new SqlParameter("@DepartmentID", id);
+            var name = new SqlParameter("@name", department.Name);
+            var Budget = new SqlParameter("@Budget", department.Budget);
+            var StartDate = new SqlParameter("@StartDate", department.StartDate);
+            var InstructorID = new SqlParameter("@InstructorID", department.InstructorId);
+            var RowVersion_Original = new SqlParameter("@RowVersion_Original", department.RowVersion);
+           
+
+            var DeptData = await _context.Department
+                .FromSqlRaw("EXECUTE dbo.Department_Update @DepartmentID,@name,@Budget,@StartDate,@InstructorID,@RowVersion_Original", parameters: new[] { id1, name, Budget, StartDate, InstructorID, RowVersion_Original })
+                .ToListAsync();
 
             return NoContent();
         }
@@ -76,13 +96,27 @@ namespace HomeWork1.Controllers
         // POST: api/Departments
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPost("{name1},{Budget1},{StartDate1},{InstructorID}")]
         [HttpPost]
         public async Task<ActionResult<Department>> PostDepartment(Department department)
         {
-            _context.Department.Add(department);
-            await _context.SaveChangesAsync();
 
+            //原本的POST內容
+            //    _context.Department.Add(department);
+            //    await _context.SaveChangesAsync();
+            //    return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+
+            //使用Stored Procedure 進行新增資料
+            var name = new SqlParameter("@name",department.Name);
+            var Budget = new SqlParameter("@Budget", department.Budget);
+            var StartDate = new SqlParameter("@StartDate", department.StartDate);
+            var InstructorID = new SqlParameter("@InstructorID",department.InstructorId);
+           
+            var DeptData = await _context.Department
+                .FromSqlRaw("EXECUTE dbo.Department_Insert @name,@Budget,@StartDate,@InstructorID",parameters: new[] { name, Budget, StartDate, InstructorID })
+                .ToListAsync();
             return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+
         }
 
         // DELETE: api/Departments/5
@@ -95,8 +129,22 @@ namespace HomeWork1.Controllers
                 return NotFound();
             }
 
-            _context.Department.Remove(department);
-            await _context.SaveChangesAsync();
+            //====原本的Delete語法====
+            //_context.Department.Remove(department);
+            //await _context.SaveChangesAsync();
+            //return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+
+            //====原本的Delete語法====
+
+
+            //使用Stored Procedure 進行刪除資料
+            var DepartmentID = new SqlParameter("@DepartmentID", department.DepartmentId);
+            var RowVersion_Original = new SqlParameter("@RowVersion_Original", department.RowVersion);
+
+            var DeptData = await _context.Department
+               .FromSqlRaw("EXECUTE dbo.Department_delete @DepartmentID,@RowVersion_Original", parameters: new[] { DepartmentID, RowVersion_Original })
+               .ToListAsync();
+           
 
             return department;
         }
